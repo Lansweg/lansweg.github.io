c119b601265bf234e2d191b750021c1fa746c2e9
Lansweg <159937804+Lansweg@users.noreply.github.com>
2026-05-20 05:57:00 +0200

    Move environment variables to config.js

---
+++ app/config.js
@@ -0,0 +1,3 @@
+const SB_URL = "https://rixsenvshgconhvgevix.supabase.co";
+const SB_KEY = "sb_publishable_9DbMPtt6ZwzQ21-ADtyB2w_V0VKAw9N";
+const MB_TOKEN = "pk.eyJ1IjoibGFuc3dlZyIsImEiOiJjbXBkaHJ6ZTYwMzQ2MnRzZTBtb25nYXl5In0._V1wF9WwBfZnxdemWuWbUQ";
