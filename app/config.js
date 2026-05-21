1cbf2074d2fc333f0054ecf43c48de1c98558969
Lansweg <159937804+Lansweg@users.noreply.github.com>
2026-05-21 12:19:42 +0200

    Delete app/config.js

--- app/config.js
+++
@@ -1,5 +0,0 @@
-window.APP_CONFIG = {
-  SB_URL: 'https://rixsenvshgconhvgevix.supabase.co',
-  SB_KEY: 'sb_publishable_9DbMPtt6ZwzQ21-ADtyB2w_V0VKAw9N',
-  MB_TOKEN: 'pk.eyJ1IjoibGFuc3dlZyIsImEiOiJjbXBkaHJ6ZTYwMzQ2MnRzZTBtb25nYXl5In0._V1wF9WwBfZnxdemWuWbUQ'
-}
